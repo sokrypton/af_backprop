7630e8d1c8b3fa5dd6d2881d7ebebce53e40345e
Sergey O <so@fas.harvard.edu>
2022-05-10 16:19:45 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, find_packages
 setup(
     name='af_backprop',
     version='0.0.0',
-    find_packages(include=['alphafold','alphafold.*'])
+    find_packages(include=['alphafold'])
 )
